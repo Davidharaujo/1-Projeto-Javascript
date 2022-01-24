64b92362b7627539803269b83c3dc5dc0b8bc5cc
Davidharaujo <araujo.davidhenrique@gmail.com>
2022-01-24 13:22:09 -0300

    primeiro-commit

---
+++ js/calcula-imc.js
@@ -0,0 +1,53 @@
+var titulo = document.querySelector(".titulo");
+
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i=0; i<pacientes.length; i++){
+  var paciente = pacientes[i];
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdImc = paciente.querySelector(".info-imc");
+
+  var pesoValida = validaPeso(peso);
+  var alturaValida = validaAltura(altura);
+
+  if(!pesoValida){
+    pesoValida=false;
+    tdImc.textContent = "Peso Inválido";
+    paciente.classList.add("paciente-invalido");
+  }
+  if(!alturaValida) {
+    alturaValida = false;
+    tdImc.textContent = "Altura Inválida";
+    paciente.classList.add("paciente-invalido");
+  }
+  if(alturaValida && pesoValida) {
+    var imc = calculaImc(peso,altura)
+    tdImc.textContent = imc
+  }
+}
+function calculaImc(peso,altura){
+  var imc=0
+  imc = peso/(altura*altura);
+  return imc.toFixed(2);
+}
+function validaPeso(peso){
+  if(peso>=0 && peso<1000){
+    return true;
+  } else{
+    return false;
+  }
+}
+function validaAltura(altura){
+  if(altura>=0 && altura<3.0){
+    return true;
+  } else{
+    return false;
+  }
+}
